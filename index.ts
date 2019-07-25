7c3f14f8ced92fde2c1b1b2a89d6225b1b6294f4
mgechev <mgechev@gmail.com>
2019-07-25 15:47:06 -0700

    Update the ng packages

--- index.ts
+++ index.ts
@@ -1,14 +1,19 @@
-import { readdirSync, statSync, readFileSync, existsSync, writeFileSync } from 'fs';
+import {
+  readdirSync,
+  statSync,
+  readFileSync,
+  existsSync,
+  writeFileSync
+} from 'fs';
 import { join } from 'path';
+import { start } from 'repl';
 
 type Package = string | [string, [string]];
 type Packages = [] | [Package];
 
 const angularPackages: any = [];
 
-const defaultPeerDeps = new Set([
-  'rxjs', 'zone.js'
-]);
+const defaultPeerDeps = new Set(['rxjs', 'zone.js']);
 
 const hasMetadata = (dir: string): boolean => {
   const dirContent = readdirSync(dir);
@@ -22,30 +27,43 @@ const hasMetadata = (dir: string): boolean => {
       return hasMetadata(join(dir, d));
     }
     return false;
-  })
+  });
 };
 
 const findAngularPackages = (startDir: string) => {
   if (!existsSync(startDir)) {
     return;
   }
-  const dirContent = readdirSync(startDir);
+  const dirContent = readdirSync(startDir).filter(dir => !dir.startsWith('.'));
   if (!dirContent.includes('package.json')) {
+    dirContent
+      .filter(c => {
+        let dir = false;
+        try {
+          dir = statSync(join(startDir, c)).isDirectory();
+        } catch {}
+        return dir;
+      })
+      .forEach(c => findAngularPackages(join(startDir, c)));
     return;
   }
-  const pkg = JSON.parse(readFileSync(join(startDir, 'package.json')).toString());
+  const pkg = JSON.parse(
+    readFileSync(join(startDir, 'package.json')).toString()
+  );
   const packageName = pkg.name;
   const packageVersion = pkg.version;
-  const peerDeps = Object.keys(pkg.peerDependencies || {}).filter((key: string) => {
-    if (defaultPeerDeps.has(key)) return false;
-    if (key.startsWith('@angular/')) return false;
-    return true;
-  }).map((p: string) => {
-    return {
-      name: p,
-      version: pkg.peerDependencies[p]
-    };
-  });
+  const peerDeps = Object.keys(pkg.peerDependencies || {})
+    .filter((key: string) => {
+      if (defaultPeerDeps.has(key)) return false;
+      if (key.startsWith('@angular/')) return false;
+      return true;
+    })
+    .map((p: string) => {
+      return {
+        name: p,
+        version: pkg.peerDependencies[p]
+      };
+    });
   if (hasMetadata(startDir)) {
     console.log('+', packageName);
     angularPackages.push({
@@ -58,14 +76,16 @@ const findAngularPackages = (startDir: string) => {
   }
   dirContent.forEach(p => {
     if (p === 'node_modules') {
-      readdirSync(join(startDir, p))
-        .forEach(c => {
-          findAngularPackages(join(startDir, p, c))
-        });
+      readdirSync(join(startDir, p)).forEach(c => {
+        findAngularPackages(join(startDir, p, c));
+      });
     }
   });
 };
 
 findAngularPackages('.');
 
-writeFileSync('angular-packages.json', JSON.stringify(angularPackages, null, 2));
+writeFileSync(
+  'angular-packages.json',
+  JSON.stringify(angularPackages, null, 2)
+);
